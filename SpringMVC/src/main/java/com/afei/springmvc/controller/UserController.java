e497969aa0af8b1ca18dcbfe687cb341153cffb2
haer1999 <haer1999@sina.com>
2017-09-14 18:03:34 +0800

    略

--- SpringMVC/src/main/java/com/afei/springmvc/controller/UserController.java
+++ SpringMVC/src/main/java/com/afei/springmvc/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.afei.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
-	
+	@RequestMapping("/getHello")
+	public ModelAndView getHello(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("message", "Hello world");
+		modelAndView.setViewName("hello");
+		return modelAndView;
+	}
 }
